f98d2e7a7f271ac3c655b73d4d23fc38627f35a1
Azorios <rosaalani99@gmail.com>
2023-05-08 14:39:19 +0200

    Update resnet50.py

--- resnet50.py
+++ resnet50.py
@@ -4,12 +4,14 @@ from torchvision.models import ResNet50_Weights
 import torch.nn as nn
 from torch.nn.modules.loss import BCEWithLogitsLoss
 import torch
+import pickle
 from torchmetrics.classification import BinaryAccuracy, BinaryF1Score, BinaryAUROC
+
 
 class ResNet50(pl.LightningModule):
     def __init__(self, lr=1e-4):
         super().__init__()
-        
+
         # load pretrained model
         self.model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
 
@@ -26,10 +28,10 @@ class ResNet50(pl.LightningModule):
 
     def forward(self, images):
         return self.model(images)
-    
+
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.fc.parameters(), lr=self.lr)
-    
+
     def training_step(self, batch, batchidx):
         images = batch['pixel_values']
         labels = batch['label'].unsqueeze(-1).float()
@@ -43,13 +45,17 @@ class ResNet50(pl.LightningModule):
         # calculate accuracy for training
         acc = self.acc(predictions, labels)
 
-        self.log('train_loss', loss, on_epoch=True)
-        self.log('train_acc', acc, on_epoch=True)
+        self.log('train_loss', loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log('train_acc', acc, on_epoch=True, logger=True)
 
         return loss
-    
+
+    #def on_train_epoch_end(self, outputs):
+        #loss = sum(output['train_loss'] for output in outputs) / len(outputs)
+        #print(loss)
+
     def validation_step(self, batch, batchidx):
-        #images, labels = batch
+        # images, labels = batch
         images = batch['pixel_values']
         labels = batch['label'].unsqueeze(-1).float()
 
@@ -62,10 +68,14 @@ class ResNet50(pl.LightningModule):
         # calculate accuracy for training
         acc = self.acc(predictions, labels)
 
-        self.log('val_loss', loss, on_epoch=True)
-        self.log('val_acc', acc, on_epoch=True)
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log('val_acc', acc, on_epoch=True, logger=True)
 
         return loss
+
+    #def on_validation_epoch_end(self, outputs):
+        #loss = sum(output['val_loss'] for output in outputs) / len(outputs)
+        #print('on_validation_epoch_end: ', loss)
 
     def test_step(self, batch, batchidx):
         images = batch['pixel_values']
@@ -77,7 +87,8 @@ class ResNet50(pl.LightningModule):
         # calculate accuracy for training
         acc = self.acc(predictions, labels)
         f1_score = self.f1_score(predictions, labels)
-        auroc= self.auroc(predictions, labels)
+        auroc = self.auroc(predictions, labels)
 
-        self.log('test_acc', acc, on_epoch=True)
-        
+        self.log('test_acc', acc, on_epoch=True, logger=True)
+        self.log('f1_score', f1_score, logger=True)
+        self.log('auroc', auroc, logger=True)
